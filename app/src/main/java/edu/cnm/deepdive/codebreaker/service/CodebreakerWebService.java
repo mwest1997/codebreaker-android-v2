d13c1800e178e79442c93c1e88f4cfc24190d626
Austin <mwest29@cnm.edu>
2020-11-24 11:49:57 -0700

    Updated WebService

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerWebService.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerWebService.java
@@ -6,7 +6,6 @@ import edu.cnm.deepdive.codebreaker.BuildConfig;
 import edu.cnm.deepdive.codebreaker.model.entity.Match;
 import edu.cnm.deepdive.codebreaker.model.entity.User;
 import io.reactivex.Single;
-import java.util.UUID;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import okhttp3.logging.HttpLoggingInterceptor.Level;
@@ -17,12 +16,14 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 
 public interface CodebreakerWebService {
 
   @GET("users/me")
   Single<User> getProfile(@Header("Authorization") String bearerToken);
+
+  @POST("matches")
+  Single<Match> startMatch(@Header("Authorization") String bearerToken, @Body Match match);
 
   static CodebreakerWebService getInstance() {
     return InstanceHolder.INSTANCE;
@@ -35,6 +36,7 @@ public interface CodebreakerWebService {
     static {
       Gson gson = new GsonBuilder()
           .excludeFieldsWithoutExposeAnnotation()
+          .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
           .create();
       HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
       interceptor.setLevel(BuildConfig.DEBUG ? Level.BODY : Level.NONE);
